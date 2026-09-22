4bf9f611f9ec2c3a8ed3fb637d73e5e2c2ae3d7e
20abhi22 <20abhi22@gmail.com>
2026-09-23 02:32:04 +0530

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals.size()==0)return true;
+        intervals.sort((a,b)->a.start-b.start);
+        int last=intervals.get(0).end;
+        for(int i=1;i<intervals.size();i++){
+            if(last>intervals.get(i).start){
+                return false;
+            }
+            last=intervals.get(i).end;
+        }
+        return true;
+    }
+}
